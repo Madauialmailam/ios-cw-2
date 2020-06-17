c4fe79839b4273deab98407e1bdc5f18028dff44
Madauialmailam <66551760+Madauialmailam@users.noreply.github.com>
2020-06-17 20:44:11 +0300

    i did it

---
+++ IOS-cw2 -p1 .playground/Contents.swift
@@ -0,0 +1,16 @@
+
+let family = ["abdullah", "Munirah", "Sarah", "Madaui"]
+
+let evens = [2, 4, 6, 8, 10, 12]
+
+let grades = [90,98,89,92,90]
+
+let trueFalse = [ true,true,true,false ]
+
+print (family[0],family[3])
+
+print (evens[0],evens[5])
+
+print (grades[0],grades[4])
+
+print (trueFalse[0],trueFalse [3])
---
+++ IOS-cw2-p2.playground/Contents.swift
@@ -0,0 +1,15 @@
+var fruits = ["mango", "orange", "potato", "peach"]
+
+print (fruits[0] , fruits[3])
+
+fruits.append ("strawberrys")
+
+fruits.remove(at:2)
+
+fruits += ["banana", "watermelon", "pineapple", "kiwi"]
+
+print(fruits)
+
+fruits.removeAll()
+
+print(fruits)
